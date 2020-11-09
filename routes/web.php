4aa55cb54f785f08176bcee01f168a9eb48d63f5
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-11-09 16:13:54 +0100

    preparazione

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+// * rotte protette Admin
+Route::prefix('admin')->name('admin.')->namespace('Admin')->middleware('auth')->group(function(){
+    Route::get('/', 'homeCointroller@index')->name('home');
+
+});
+
+// * rotte non protette Guest
